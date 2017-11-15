5b99e894fb6823bb64e313a4df598fa24289d82e
Osher E <osher@adtechium.com>
2017-11-15 15:16:00 +0200

    move packages to ws

---
+++ ws/orm/index.js
@@ -0,0 +1,10 @@
+const logger = require('logger')
+
+
+exports.declare = (name) => {
+    const data = {}
+    return {
+      get: (id) => logger.info('getting', id, data[id]) || data[id] || null,
+      set: (id, value) => logger.info('setting id %s to', id, value) || (data[id] = value)
+    }
+}
